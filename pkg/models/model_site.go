a7bd67a8cf0e2ced2568256254549dd7420e4f66
toshski <toshski8760@gmail.com>
2024-07-01 19:19:24 +1200

    Fix downloads using search from channels query

--- pkg/models/model_site.go
+++ pkg/models/model_site.go
@@ -19,6 +19,7 @@ type Site struct {
 	LimitScraping  bool      `json:"limit_scraping" xbvrbackup:"limit_scraping"`
 	MasterSiteID   string    `json:"master_site_id" xbvrbackup:"master_site_id"`
 	MatchingParams string    `json:"matching_params" gorm:"size:1000" xbvrbackup:"matching_params"`
+	Adulttime      string    `json:"adulttime" gorm:"text" xbvrbackup:"adulttime"`
 }
 
 func (i *Site) Save() error {
