06e94b06b9d5c6207f942228ff570ab86ff653bf
toshski <toshski8760@gmail.com>
2024-07-25 07:09:50 +1200

    Stash ACtor Id in HSP

--- pkg/api/heresphere.go
+++ pkg/api/heresphere.go
@@ -588,6 +588,17 @@ func getHeresphereSceneData(sceneID string, req *restful.Request, scanOnly bool)
 				Name: "Talent:" + scene.Cast[i].Name,
 			})
 		}
+		var extreflinks []models.ExternalReferenceLink
+		db.Preload("ExternalReference").Where(&models.ExternalReferenceLink{InternalTable: "actors", InternalDbId: scene.Cast[i].ID, ExternalSource: "stashdb performer"}).Find(&extreflinks)
+		for _, link := range extreflinks {
+			var stashPerf models.StashPerformer
+			json.Unmarshal([]byte(link.ExternalReference.ExternalData), &stashPerf)
+			tagName := stashPerf.Name
+			if stashPerf.Disambiguation != "" {
+				tagName += " (" + stashPerf.Disambiguation + ")"
+			}
+			tags = append(tags, HeresphereTag{Name: "Stash Actor:" + tagName})
+		}
 	}
 	if (len(scene.Cast) - akaCnt) > 5 {
 		addFeatureTag("Cast: 6+")
