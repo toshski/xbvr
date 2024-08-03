478dd7884245c2b50fa8a4df7fdcfa22a3930371
toshski <toshski8760@gmail.com>
2024-08-04 06:48:31 +1200

    finished refactoring to remove autocomplete and tidy up

--- ui/src/views/actors/SearchStashdbActors.vue
+++ ui/src/views/actors/SearchStashdbActors.vue
@@ -1,8 +1,3 @@
-<!--
-   accept query string back  
-
-   add a rescan option for users entering their own query, try to validate
--->
 <template>
   <b-modal :active="isModalActive"           
            has-modal-card
@@ -13,113 +8,59 @@
            can-cancel>
     
 
-    <div class="modal-card" id="test" style="height: 80vh; width: 60vw; left: 10vw">
+    <div class="modal-card" style="height: 80vh; width: 60vw; left: 10vw">
       <header class="modal-card-head">
         <p class="modal-card-title">Search Stashdb Actors</p>
         <button class="delete" @click="close" aria-label="close"></button>
       </header>
 
       <div class="modal-card-body">
-            <div >
-                <!-- <div><span class="has-text-danger is-small">warnindgS</span></div>                 -->
-                  <b-field label="Find actor...">
-      <b-input
-        v-model="queryString"
-        placeholder="Find actor..."
-        @input="debouncedSearch"
-        :loading="isFetching"
-        custom-class="is-large"
-      ></b-input>
-    </b-field>
+        <div >
+          <b-field label="Find actor...">
+            <b-input v-model="queryString" placeholder="Find actor..." @input="debouncedSearch" :loading="isFetching" custom-class="is-large"/>
+          </b-field>
     
-    <b-table :data="searchResults" >
-      <b-table-column field="Name" >
-        <template slot-scope="props">
-          <div class="media">
-            <div class="media-left">
-              <vue-load-image height="50px">
-                <img slot="image" :src="props.row.ImageUrl && props.row.ImageUrl.length ? getImageURL(props.row.ImageUrl[0]) : '/ui/images/blank_female_profile.png'" width="100" />
-                <img slot="preloader" src="/ui/images/blank.png" width="100" />
-                <img slot="error" src="/ui/images/blank.png" width="100" />
-              </vue-load-image>
-              <div v-if="props.row.DOB">
-                <small>
-                  <strong>Birth Date:</strong> {{ format(parseISO(props.row.DOB), "yyyy-MM-dd") }}
-                </small>
+        <b-table :data="searchResults" >
+          <b-table-column field="Name" >
+            <template slot-scope="props">
+              <div class="media">
+                <div class="media-left">
+                  <vue-load-image height="50px">
+                    <img slot="image" :src="props.row.ImageUrl && props.row.ImageUrl.length ? getImageURL(props.row.ImageUrl[0]) : '/ui/images/blank_female_profile.png'" width="100" />
+                    <img slot="preloader" src="/ui/images/blank.png" width="100" />
+                    <img slot="error" src="/ui/images/blank.png" width="100" />
+                  </vue-load-image>
+                  <div v-if="props.row.DOB">
+                    <small>
+                      <strong>Birth Date:</strong> {{ format(parseISO(props.row.DOB), "yyyy-MM-dd") }}
+                    </small>
+                  </div>
+                  <div>
+                    <small>
+                      <strong>Score:</strong> {{ props.row.Weight }}
+                    </small>
+                  </div>
+                  <div>
+                    <a class="button is-primary is-small" @click="linktoStashdb(props.row)" :title="'Link Actor with stashdb'">
+                      <b-icon pack="mdi" :icon="'link-variant-plus'" size="is-small" />
+                    </a>
+                  </div>
+                </div>
+                <div class="media-content">
+                  <div class="truncate">
+                    <strong>
+                      <a :href="props.row.Url" target="_blank">{{ props.row.Name }} - {{ props.row.Disambiguation }}</a>
+                    </strong>
+                  </div>
+                </div>
               </div>
-              <div>
-                <small>
-                  <strong>Score:</strong> {{ props.row.Weight }}
-                </small>
-              </div>
-              <div>
-                <a class="button is-primary is-small" @click="linktoStashdb(props.row)" :title="'Link Actor with stashdb'">
-                  <b-icon pack="mdi" :icon="'link-variant-plus'" size="is-small" />
-                </a>
-              </div>
-            </div>
-            <div class="media-content">
-              <div class="truncate">
-                <strong>
-                  <a :href="props.row.Url" target="_blank">{{ props.row.Name }} - {{ props.row.Disambiguation }}</a>
-                </strong>
-              </div>
-            </div>
-          </div>
-        </template>
-      </b-table-column>
-    </b-table>
-                    <b-field v-if="false">                                      
-                    <b-autocomplete
-                        ref="autocompleteInput"
-                        :data="searchResults"
-                        placeholder="Find actor..."
-                        field="query"
-                        :loading="isFetching"
-                        v-model="queryString"
-                        @typing="searchStashdb"
-                        @select="option => selectActor(option)"
-                        :open-on-focus="true"
-                        custom-class="is-large"
-                        max-height="70vh">
-
-                        <template slot-scope="props" >
-                            <div class="media">
-                                <div class="media-left">
-                                    <vue-load-image>
-                                        <img slot="image" :src="props.option.ImageUrl!=null && props.option.ImageUrl.length !=0 ? getImageURL(props.option.ImageUrl[0]) : '/ui/images/blank_female_profile.png'" height="150" width="200"/>
-                                        <img slot="preloader" src="/ui/images/blank.png" height="150" width="200"/>
-                                        <img slot="error" src="/ui/images/blank.png" height="150"  width="200"/>
-                                    </vue-load-image>
-                                  <div v-if="props.option.DOB!=''"><small><strong>Birth Date:</strong> {{format(parseISO(props.option.DOB), "yyyy-MM-dd")}}</small></div>                                  
-                                  <div><small><strong>Score:</strong> {{ props.option.Weight }}</small></div>
-                                  <div>
-                                    <a class="button is-primary is-small" @click="linktoStashdb(props.option)" :title="'Link Actor with stashdb'">
-                                      <b-icon pack="mdi" :icon="'link-variant-plus'" size="is-small"/>
-                                    </a>
-                                  </div>
-                                </div>
-                                <div class="media-content">
-                                     <div class="truncate"><strong><a :href="props.option.Url"  target="_blank">{{ props.option.Name }} - {{ props.option.Disambiguation }}</a></strong></div>                                                                        
-<!--                                    <div style="margin-top:0.5em">                                        
-                                        <small style="white-space: normal; display: block;">
-                                            <span v-for="(c, idx) in props.option.Performers" :key="'Performers' + idx">
-                                                {{c.Name}}<span v-if="idx < props.option.Performers.length-1">, </span>
-                                            </span>
-                                        </small>
-                                    </div>
- -->                            </div>
-                                </div>            
-                        </template>
-                    </b-autocomplete>
-                </b-field>
-
-            </div>
-        
+            </template>
+          </b-table-column>
+        </b-table>
       </div>
-
-      <footer class="modal-card-foot">
-      </footer>
+    </div>
+    <footer class="modal-card-foot">
+    </footer>
     </div>
   </b-modal>
 </template>
@@ -147,7 +88,6 @@ export default {
         isModalActive: true,
         stashdbUrl: "",
         searchResults: [],
-        query: "",
         queryString: "",
         isFetching: false,
         actor: "",
@@ -158,27 +98,19 @@ export default {
   },
   mounted () {
     const item = Object.assign({}, this.$store.state.overlay.searchStashDbActors.actor)    
-    console.log("insearch stash")
-    console.log(item)
     this.actor = item
     this.openDialog(item)
     this.queryString=this.actor.name
-    this.query=this.actor.name
   },
   methods: {
     format,
     parseISO,
     close () {
-      console.log("close")
       this.$store.commit('overlay/hideSearchStashdbActors')
     },
     searchStashdb() {
-        console.log("in searchStashdb", this.queryString)        
-        console.log("in searchStashdb this ", this)        
-        console.log("actor ", this.actor)        
         ky.get('/api/extref/stashdb/searchactor/' + this.actor.id + "?q=" + this.queryString, {timeout: 6e6}).json().then(data => {
             this.searchResults = Object.values(data.Results).sort((a, b) => b.Weight - a.Weight)
-            console.log(this.searchResults)
             this.isModalActive = true
             if (data.Status!='') {
               this.$buefy.toast.open({message: `Warning:  ${data.Status}`, type: 'is-warning', duration: 5000})
@@ -186,21 +118,15 @@ export default {
         })
     },
     selectActor(option) {
-        console.log("in select actor")
-        console.log(option)
         this.stashdbUrl=option.Url.replace("https://stashdb.org/performers/","")
         this.$nextTick(() => {
-          console.log("nextTick", this.isModalActive)
             if (this.$refs.autocompleteInput) {
                 this.$refs.autocompleteInput.focus();
             }
         });
     },    
     linktoStashdb(option) {
-        console.log("in linktoStashdb")
-        console.log(option)
         this.stashdbUrl=option.Url.replace("https://stashdb.org/performers/","")
-        console.log('/api/extref/stashdb/link2actor/' + this.actor.id +'/'+this.stashdbUrl)
         ky.get('/api/extref/stashdb/link2actor/' + this.actor.id +'/'+this.stashdbUrl ).json().then(data => {          
           // this.$store.commit('sceneList/updateScene', data)
            this.$store.commit('overlay/showActorDetails', { actor: data })
@@ -208,7 +134,6 @@ export default {
         })
     },    
     getImageURL (u) {        
-      console.log("getimage",u)
       if (u != undefined && u.startsWith('http')) {
         return '/img/120x/' + u.replace('://', ':/')
       } else {
@@ -216,22 +141,15 @@ export default {
       }
     },
     openDialog(actor) {
-      console.log("in openDialogOld")
         this.isModalActive = true
-        console.log("openDialogOld isModalActive", this.isModalActive)
         this.searchStashdb()
-        console.log("openDialogOld2 isModalActive", this.isModalActive)
         this.$nextTick(() => {
-          console.log("nextTick", this.isModalActive)
             if (this.$refs.autocompleteInput) {
                 this.$refs.autocompleteInput.focus();
             }
         });
         this.actor = actor
     },
-    dump(p) {
-console.log("********** dump",p)
-    }
   },
   computed: {
 
