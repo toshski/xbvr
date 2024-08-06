1a7d335ee9c909397b193a859168d04b3b98815b
toshski <toshski8760@gmail.com>
2024-08-07 10:52:39 +1200

    Manually Link to StashDb

---
+++ ui/src/components/LinkStashdbButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <span>
+        <a :class="buttonClass"
+            @click="openDialog(item)"
+            :title="'Link to stashdb'">
+            <b-icon pack="mdi" :icon="'link-variant-plus'" size="is-small"/>
+        </a>
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'LinkStashdbButton',
+  props: { item: Object, objectType: String },  
+  data () {
+    return {
+        isModalActive: false,
+        searchResults: [],
+        query: "",
+        queryString: "",
+        isFetching: false,
+        }
+  },
+  computed: {
+    buttonClass () {
+      return 'button  is-outlined is-small'
+    }
+  },
+  methods: {
+    openDialog(item) {
+        console.log("stash button open", this)
+        if (this.objectType == "scene") {
+      this.$store.commit('overlay/showSearchStashdbScenes', { item })
+        } else {
+this.$store.commit('overlay/showSearchStashdbActors', { item })
+        }
+    },
+     }
+}
+</script>
