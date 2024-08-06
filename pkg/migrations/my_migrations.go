5ade6a849af9865bda72c63a0902753916c81e7a
toshski <toshski8760@gmail.com>
2024-08-07 09:52:30 +1200

    Migration Actor Site Riles Config File

--- pkg/migrations/my_migrations.go
+++ pkg/migrations/my_migrations.go
@@ -1,6 +1,10 @@
 package migrations
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
 	"github.com/jinzhu/gorm"
 	"github.com/xbapps/xbvr/pkg/common"
 	"github.com/xbapps/xbvr/pkg/models"
@@ -84,9 +88,50 @@ func MyMigrate() {
 			},
 		},
 		{
-			ID: "0013",
+			ID: "0013-actor_scraper_config",
 			Migrate: func(tx *gorm.DB) error {
-				return nil
+				type actorScraperConfig struct {
+					StashSceneMatching         map[string]models.StashSiteConfig
+					GenericActorScrapingConfig map[string]models.GenericScraperRuleSet
+				}
+
+				fName := filepath.Join(common.AppDir, "actor_scraper_custom_config.json")
+				if _, err := os.Stat(fName); os.IsNotExist(err) {
+					return nil
+				}
+				var newCustomScrapeRules models.ActorScraperConfig
+				b, err := os.ReadFile(fName)
+				if err != nil {
+					return err
+				}
+				e := json.Unmarshal(b, &newCustomScrapeRules)
+				if e == nil {
+					// if we can read the file with the new model, it has already been converted
+					return nil
+				}
+
+				var oldCustomScrapeRules actorScraperConfig
+				e = json.Unmarshal(b, &oldCustomScrapeRules)
+				if e != nil {
+					// can't read the old layout either ?
+					return err
+				}
+
+				newCustomScrapeRules = models.ActorScraperConfig{}
+				newCustomScrapeRules.GenericActorScrapingConfig = oldCustomScrapeRules.GenericActorScrapingConfig
+				newCustomScrapeRules.StashSceneMatching = map[string][]models.StashSiteConfig{}
+				for key, scraper := range oldCustomScrapeRules.StashSceneMatching {
+					common.Log.Infof("%s %s", key, scraper)
+					common.Log.Infof("%s", oldCustomScrapeRules.StashSceneMatching[key])
+					newScraperCofig := oldCustomScrapeRules.StashSceneMatching[key]
+					common.Log.Infof("%s", newScraperCofig)
+					newCustomScrapeRules.StashSceneMatching[key] = []models.StashSiteConfig{}
+					newCustomScrapeRules.StashSceneMatching[key] = append(newCustomScrapeRules.StashSceneMatching[key], newScraperCofig)
+				}
+				out, _ := json.MarshalIndent(newCustomScrapeRules, "", "  ")
+				e = os.WriteFile(fName, out, 0644)
+				return e
+
 			},
 		},
 		{
